870d7bd77716f17b037f2a626eefd8876b9d9763
joelle_walley <jojowalley@berkeley.edu>
2021-04-09 15:04:29 -0700

    done

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # oop_game-v2
  Project Overview In this project, you'll create a browser-based, word guessing game: "Phrase Hunter." You’ll use JavaScript and OOP (Object-Oriented Programming) to select a random, hidden phrase, which a player tries to guess, by clicking letters on an onscreen keyboard.  Using JavaScript, you’ll create two JavaScript classes with specific properties and methods. You'll create a Game class for managing the game, and a Phrase class to help with creating an array of Phrase objects.  Your code will choose a random phrase, split the phrase into letters, and put those letters onto the gameboard.  Each time the player guesses a letter, the program compares the letter the player has chosen with the random phrase. If the letter is in the phrase, the gameboard displays the chosen letters on the screen.  A player continues to select letters until they guess the phrase (and win), or make five incorrect guesses (and lose).  If the player completes the phrase before they run out of guesses, a winning screen appears. If the player guesses incorrectly five times, a losing screen appears.  A player can guess a letter only once. After they’ve guessed a letter, your programming will need to disable that letter on the onscreen keyboard.
+
+
+"Exceeds"
+
+Added keyboard functionality: 
+I added it so that the user can type the letters from their keyboard to guess the phrase.
+
+Styling - experimenting with colors.
+I changed the title color and the show letter that's displayed color as well.
+
+The general layout should remain the same, but feel free to make the project your own by experimenting with things like color, background color, font, borders, shadows, transitions, animations, filters, etc.
--- js/app.js
+++ js/app.js
@@ -56,13 +56,157 @@ document.getElementById('btn__reset').addEventListener('click', (e) => {
 });
 
 const keySelectors = document.querySelectorAll('.key');
+// const keysInDom = document.getElementById('qwerty');
 
 for (let i = 0; i < keySelectors.length; i++) {
     keySelectors[i].addEventListener('click', e => {
         game.handleInteraction(e.target);
     });
-    
 }
+
+document.addEventListener('keypress', e => {
+    if (!game) {
+        return;
+    }
+    for (let i = 0; i < keySelectors.length; i++) {
+        if (keySelectors[i].innerHTML === e.key) {
+            keySelectors[i].click();
+        }
+    }
+});
+
+// keySelectors.forEach(letterOfButton => {
+//     letterOfButton.addEventListener('click', function() {
+//         game.handleInteraction(this);
+//     });
+// });
+
+// document.addEventListener('keyup', ({lettersOfKey}) => {
+//     document.querySelectorAll('key').forEach(letterOfButton => {
+//         if (letterOfButton.textContent === lettersOfKey && !letterOfButton.disabled) {
+//             game.handleInteraction(letterOfButton);
+//         }
+//     });
+// });
+
+// const firstRow = keysInDom[0];
+// firstRow.addEventListener('click', () => {
+//     game.handleInteraction(firstRow);
+// });
+// const firstRow1 = keysInDom[1];
+// firstRow.addEventListener('click', () => {
+//     game.handleInteraction(firstRow1);
+// });
+// const firstRow2 = keysInDom[2];
+// firstRow2.addEventListener('click', () => {
+//     game.handleInteraction(firstRow2);
+// });
+// const firstRow3 = keysInDom[3];
+// firstRow3.addEventListener('click', () => {
+//     game.handleInteraction(firstRow3);
+// });
+// const firstRow4 = keysInDom[4];
+// firstRow4.addEventListener('click', () => {
+//     game.handleInteraction(firstRow4);
+// });
+// const firstRow5 = keysInDom[5];
+// firstRow5.addEventListener('click', () => {
+//     game.handleInteraction(firstRow5);
+// });
+// const firstRow6 = keysInDom[6];
+// firstRow6.addEventListener('click', () => {
+//     game.handleInteraction(firstRow6);
+// });
+// const firstRow7 = keysInDom[7];
+// firstRow7.addEventListener('click', () => {
+//     game.handleInteraction(firstRow7);
+// });
+// const firstRow8 = keysInDom[8];
+// firstRow8.addEventListener('click', () => {
+//     game.handleInteraction(firstRow8);
+// });
+// const firstRow9 = keysInDom[9];
+// firstRow9.addEventListener('click', () => {
+//     game.handleInteraction(firstRow9);
+// });
+
+// const secondRow = keysInDom[10];
+// secondRow.addEventListener('click', () => {
+//     game.handleInteraction(secondRow);
+// });
+// const secondRow1 = keysInDom[11];
+// secondRow1.addEventListener('click', () => {
+//     game.handleInteraction(secondRow1);
+// });
+// const secondRow2 = keysInDom[12];
+// secondRow2.addEventListener('click', () => {
+//     game.handleInteraction(secondRow2);
+// });
+// const secondRow3 = keysInDom[13];
+// secondRow3.addEventListener('click', () => {
+//     game.handleInteraction(secondRow3);
+// });
+// const secondRow4 = keysInDom[14];
+// secondRow4.addEventListener('click', () => {
+//     game.handleInteraction(secondRow4);
+// });
+// const secondRow5 = keysInDom[15];
+// secondRow5.addEventListener('click', () => {
+//     game.handleInteraction(secondRow5);
+// });
+// const secondRow6 = keysInDom[16];
+// secondRow6.addEventListener('click', () => {
+//     game.handleInteraction(secondRow6);
+// });
+// const secondRow7 = keysInDom[17];
+// secondRow7.addEventListener('click', () => {
+//     game.handleInteraction(secondRow7);
+// });
+// const secondRow8 = keysInDom[18];
+// secondRow8.addEventListener('click', () => {
+//     game.handleInteraction(secondRow8);
+// });
+
+// const thirdRow = keysInDom[19];
+// thirdRow.addEventListener('click', () => {
+//     game.handleInteraction(thirdRow);
+// });
+
+// const thirdRow1 = keysInDom[20];
+// thirdRow1.addEventListener('click', () => {
+//     game.handleInteraction(thirdRow1);
+// });
+
+// const thirdRow2 = keysInDom[21];
+// thirdRow2.addEventListener('click', () => {
+//     game.handleInteraction(thirdRow2);
+// });
+
+// const thirdRow3 = keysInDom[22];
+// thirdRow3.addEventListener('click', () => {
+//     game.handleInteraction(thirdRow3);
+// });
+
+// const thirdRow4 = keysInDom[23];
+// thirdRow4.addEventListener('click', () => {
+//     game.handleInteraction(thirdRow4);
+// });
+
+// const thirdRow5 = keysInDom[24];
+// thirdRow5.addEventListener('click', () => {
+//     game.handleInteraction(thirdRow5);
+// });
+
+// const thirdRow6 = keysInDom[25];
+// thirdRow6.addEventListener('click', () => {
+//     game.handleInteraction(thirdRow6);
+// });
+
+// const thirdRow7 = keySelectors[26];
+// thirdRow7.addEventListener('click', () => {
+//     game.handleInteraction(thirdRow7);
+// });
+
 // document.querySelectorAll('.key').addEventListener('click', e => {
 //     console.log(e.target.value);
 //     game.handleInteraction(e.target);
